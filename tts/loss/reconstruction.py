49c555331bdd0dfbc5abfde7cdcb5a850fedb7d5
Timofey S <timothyxp@mail.ru>
2021-12-14 18:47:55 +0300

    add padding

--- tts/loss/reconstruction.py
+++ tts/loss/reconstruction.py
@@ -4,19 +4,24 @@ from torch.nn import functional as F
 
 
 class WaveFormReconstructionLoss(nn.Module):
-    def __init__(self, featurizer):
+    def __init__(self, featurizer, pad_value):
         super().__init__()
         self.waveform_loss = nn.L1Loss()
         self.featurizer = featurizer
+        self.pad_value = pad_value
 
     def forward(self, batch: Batch) -> Tensor:
         melspec_prediction = self.featurizer(batch.waveform_prediction)
+        melspec = batch.melspec
 
-        # diff_len = batch.waveform_prediction.shape[-1] - batch.waveform.shape[-1]
-        # waveform = F.pad(batch.waveform, (0, diff_len))
+        diff_len = melspec_prediction.shape[-1] - batch.melspec.shape[-1]
+        if diff_len > 0:
+            melspec_prediction = F.pad(melspec_prediction, (0, diff_len), self.pad_value)
+        else:
+            melspec = F.pad(melspec, (0, -diff_len), self.pad_value)
 
         waveform_l1 = self.waveform_loss(
-            batch.melspec,
+            melspec,
             melspec_prediction
         )
 
