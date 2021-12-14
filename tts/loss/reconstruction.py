0a54401b536070e93513cbd7bbea2888732fd255
Timofey S <timothyxp@mail.ru>
2021-12-14 18:35:48 +0300

    fix loss

--- tts/loss/reconstruction.py
+++ tts/loss/reconstruction.py
@@ -4,17 +4,20 @@ from torch.nn import functional as F
 
 
 class WaveFormReconstructionLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, featurizer):
         super().__init__()
         self.waveform_loss = nn.L1Loss()
+        self.featurizer = featurizer
 
     def forward(self, batch: Batch) -> Tensor:
-        diff_len = batch.waveform_prediction.shape[-1] - batch.waveform.shape[-1]
-        waveform = F.pad(batch.waveform, (0, diff_len))
+        melspec_prediction = self.featurizer(batch.waveform_prediction)
+
+        # diff_len = batch.waveform_prediction.shape[-1] - batch.waveform.shape[-1]
+        # waveform = F.pad(batch.waveform, (0, diff_len))
 
         waveform_l1 = self.waveform_loss(
-            batch.waveform_prediction,
-            waveform
+            batch.melspec,
+            melspec_prediction
         )
 
         return waveform_l1
